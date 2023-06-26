31f8becfe22121180c0ef0866e479dcb60e0b9dc
Nraihpuael <nahuelripa@hotmail.com>
2023-06-26 02:14:34 -0400

    four commit

--- resources/views/diagramas/diagramar.blade.php
+++ resources/views/diagramas/diagramar.blade.php
@@ -45,6 +45,22 @@
                                 </div>
                             </div>
 
+                            <div class="col-12 col-md-auto ms-auto d-print-none">
+                           
+                                <a href="#" class="btn btn-white d-none d-sm-inline-block" data-bs-toggle="modal"
+                                    data-bs-target="#modal-report">
+                                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <line x1="12" y1="5" x2="12" y2="19" />
+                                        <line x1="5" y1="12" x2="19" y2="12" />
+                                    </svg>
+                                    Indicaciones
+                                </a>
+                            </div>
+                            
                             <div class="col-auto ">
                                 <a href="{{route('diagramas.misDiagramas')}}"
                                     class="btn btn-white">
@@ -67,6 +83,22 @@
                     </div>
 
 
+                </div>
+            </div>
+        </div>
+        <div class="modal modal-blur fade" id="modal-report" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">
+                            Seguir la Sintaxis nombre_de_atributo tipo_atributo<br>
+                            Si quiere definir llave primaria luego del tipo_atributo escriba PK<br>
+                            Si es llave Foranea escriba FK
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                    </div>
+                    
                 </div>
             </div>
         </div>
